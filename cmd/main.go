70e0fc4e52f81df96d2f934638cd52bd839f78fd
AbdulBari <bay7amov@gmail.com>
2025-03-10 17:52:41 +0300

    added connect db in file main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,70 @@
 package main
 
-func main() {
+import (
+	"film-library/internal/config"
+	"film-library/internal/lib/logger/handlers/slogpretty"
+	"film-library/internal/lib/logger/sl"
+	"film-library/internal/storage/postgres"
+	"log/slog"
+	"os"
+)
 
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting film-library", slog.String("env", cfg.Env),
+		slog.String("version", "123"),
+	)
+	log.Debug("debug messages are enabled")
+
+	storage, err := postgres.Connect(cfg.Database)
+	if err != nil {
+		log.Error("failed to init storage", "error: %v", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		// log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog() // здесь преукрасили вывод логов для удобства
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	default:
+		panic("not supported env")
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
